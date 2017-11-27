e5844cf6d26c1fabe2f88e5104a1263b8b67c649
lmc2179 <louiscialdella@gmail.com>
2017-11-26 22:10:52 -0800

    refactored histogram entropy

--- query_by_bagging/histogram_entropy.py
+++ query_by_bagging/histogram_entropy.py
@@ -1,10 +1,21 @@
 import numpy as np
 
+def histogram_entropy(X, l, r, nbins=10):
+    hist, bin_edges = np.histogram(X, nbins, (l, r))  # What is the appropriate bin choice? Constant-width? Shared data FD?
+    w = (r - l) / nbins
+    total_points = 1.0 * sum(hist)
+    p = hist / total_points
+    p = np.array([prob for prob in p if prob > 0])
+    ent = -sum(p * np.log(p / w))
+    return ent
+
+def multiple_histogram_entropy(X_samples, nbins=10):
+    l = min([min(x) for x in X_samples])
+    r = max([max(x) for x in X_samples])
+    return [histogram_entropy(x, l, r, nbins=nbins) for x in X_samples]
+
 X = np.random.uniform(0, 100, 1000)
-hist, bin_edges = np.histogram(X, 10) # What is the appropriate bin choice? Constant-width? Shared data FD?
-w = bin_edges[1] - bin_edges[0]
-total_points = 1.0 * sum(hist)
-p = hist / total_points
-ent = -sum(p * np.log(p / w))
-print(ent)
+print(histogram_entropy(X, 0, 100, nbins=100))
+X2 = np.random.uniform(300, 400, 1000)
+print(multiple_histogram_entropy([X, X2], 10))
 print(np.log(100))
